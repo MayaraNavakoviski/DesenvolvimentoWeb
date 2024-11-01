d02ac05862fd8c32ed10a78430e33d2a480f71a2
Mayara Navakoviski <mayara.navakoviski.tds2023@gmail.com>
2024-11-01 15:12:43 -0300

    Create script.js

---
+++ TerceiroBimestre/script.js
@@ -0,0 +1,11 @@
+
+
+const input = document.querySelector('input');
+const botao = document.querySelector('button');
+const lista = document.querySelector('ul');
+
+botao.addEventListener('click', ()=> {
+
+    let novaTarefa = document.createElement('li')
+
+})
